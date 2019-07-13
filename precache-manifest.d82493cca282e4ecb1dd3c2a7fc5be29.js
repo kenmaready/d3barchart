f4ed9cda72b3a7c1336b644753a684b521def10d
kenmaready <ken@maready.com>
2019-07-13 12:25:21 -0400

    Updates

---
+++ precache-manifest.d82493cca282e4ecb1dd3c2a7fc5be29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be7e1a240230a570d225e7ded2ae0536",
+    "url": "/d3barchart/index.html"
+  },
+  {
+    "revision": "b3a3594b1eb7a13e8015",
+    "url": "/d3barchart/static/css/main.1c3fd4df.chunk.css"
+  },
+  {
+    "revision": "7b911c3c6fbc5874ea99",
+    "url": "/d3barchart/static/js/2.0eb06809.chunk.js"
+  },
+  {
+    "revision": "b3a3594b1eb7a13e8015",
+    "url": "/d3barchart/static/js/main.1ed0bee5.chunk.js"
+  },
+  {
+    "revision": "57b55baca958a4c29025",
+    "url": "/d3barchart/static/js/runtime~main.d2deea42.js"
+  }
+]);
